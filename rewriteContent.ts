0343753d7419da52b9f27906dc21b47b33ed2ebb
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2024-02-23 11:28:19 +0100

    feat: add `rewriteContent()` function

---
+++ rewriteContent.ts
@@ -0,0 +1,21 @@
+const REGEX =
+  /(?:(?<=(?:import|export)[^`'"]*from\s+[`'"])(?<path1>[^`'"]+)(?=(?:'|"|`)))|(?:\b(?:import|export)(?:\s+|\s*\(\s*)[`'"](?<path2>[^`'"]+)[`'"])/g
+
+/**
+ * Rewrite imports and exports in a string.
+ * 
+ * @example
+ * 
+ * ```ts
+ * let fileContent = await Deno.readTextFile('./file.js')
+ * 
+ * fileContent = rewriteContent(fileContent, ({ url }) => {
+ *   return url.replace('https://esm.sh/', 'npm:')
+ * })
+ * ```
+ */
+export function rewriteContent(fileContent: string, replacer: (data: { url: string }) => string) {
+  return fileContent.replace(REGEX, (_, url) => {
+    return replacer({ url })
+  })
+}
